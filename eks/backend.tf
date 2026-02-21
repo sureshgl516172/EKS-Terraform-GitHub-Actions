55afb6772c0e692e082d7b3dca09df3616d278d7
sureshgl516172 <155871859+sureshgl516172@users.noreply.github.com>
2026-02-21 13:24:26 +0530

    Update S3 bucket name in backend configuration

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "dev-aman-555678845265-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
